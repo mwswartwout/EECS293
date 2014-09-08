8494fc049ee85c2e6d64784516b92208feba01fe
Matthew Swartwout <mws85@case.edu>
2014-09-07 23:56:48 -0400

    Blank Methods

---
+++ hw2/src/hw2/LinkedWith.java
@@ -0,0 +1,11 @@
+package hw2;
+
+public class LinkedWith 
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
---
+++ hw2/src/hw2/User.java
@@ -0,0 +1,34 @@
+package hw2;
+
+public class User 
+{
+	private String ID;
+	private enum validity { VALID, INVALID };
+	private validity status;
+	
+	public User()
+	{
+		status = validity.INVALID;
+	}
+	
+	public boolean setID(String id)
+	{
+		
+	}
+	
+	public String getID()
+	{
+		
+	}
+	
+	public boolean isValid()
+	{
+		
+	}
+	
+	public String toString()
+	{
+		
+	}
+	
+}

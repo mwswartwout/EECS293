16437dd59786363c07094f4026c81439b0f910cd
Matthew Swartwout <mws85@case.edu>
2014-09-08 22:23:10 -0400

    User Update
    Completed all of the User methods.
    Turned Validity into a stand-alone enum.

---
+++ hw2/src/hw2/Validity.java
@@ -0,0 +1,13 @@
+package hw2;
+
+/**
+ * Enum {@code Validity} represents whether a user is valid or invalid
+ * 
+ * @author Matthew Swartwout
+ *
+ */
+public enum Validity 
+{
+	VALID,
+	INVALID;
+}
